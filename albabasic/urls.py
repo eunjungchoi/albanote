28cac875b801fb910d8ac62042ee89748c58f154
Eun Jung Choi <im.your.energy@gmail.com>
2020-01-27 15:05:57 +0900

    Refactor directory

--- albabasic/urls.py
+++ albabasic/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from albabasic.albabasic import views
+from albabasic import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
