b861e45b532f6f381a1bf2f5b9d0dde0f2e69b03
Eun Jung Choi <im.your.energy@gmail.com>
2020-01-28 16:49:37 +0900

    Fix cors list url

--- albabasic/settings.py
+++ albabasic/settings.py
@@ -182,7 +182,7 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:8080',
     'http://127.0.0.1:8080',
-    '172.31.18.211'
+    'http://172.31.18.211'
 ]
 
 APPEND_SLASH = True
