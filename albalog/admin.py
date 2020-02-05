90a4ec95092ee7c115db94d89aa45c3bdbab7631
Eun Jung Choi <im.your.energy@gmail.com>
2020-02-05 17:05:06 +0900

    Add holiday policy and payroll on admin site

--- albalog/admin.py
+++ albalog/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 from django.contrib.auth.admin import UserAdmin
 
-from albalog.models import User, Business, Member, TimeTable, Attendance
+from albalog.models import User, Business, Member, TimeTable, Attendance, HolidayPolicy, PayRoll
 
 
 class CustomeUserAdmin(UserAdmin):
@@ -22,7 +22,7 @@ class BusinessAdmin(ModelAdmin):
 
 
 class MemberAdmin(ModelAdmin):
-    list_display = ('id', 'business_name', 'user', 'member_name', 'type', 'hourly_wage')
+    list_display = ('id', 'business_name', 'user', 'member_name', 'type', 'hourly_wage', 'weekly_holiday', 'start_date', 'annual_leave')
 
     def business_name(self, obj):
         return obj.business.license_name
@@ -32,11 +32,27 @@ class MemberAdmin(ModelAdmin):
 
 
 class AttendanceAdmin(ModelAdmin):
-    list_display = ('id', 'member', 'start_time', 'end_time', 'duration')
+    list_display = ('id', 'member', 'start_time', 'end_time', 'duration', 'date', 'absence', 'reason')
 
 
 class TimeTableAdmin(ModelAdmin):
     list_display = ('id', 'member_name', 'day', 'start_time', 'end_time')
+
+    def member_name(self, obj):
+        return obj.member.business.license_name + '_' + obj.member.user.name
+
+
+class HolidayPolicyAdmin(ModelAdmin):
+    list_display = ('id', 'business_name', 'type_name', 'paid', 'memo')
+
+    def business_name(self, obj):
+        return obj.business.license_name
+
+    def type_name(self, obj):
+        return obj.type
+
+class PayrollAdmin(ModelAdmin):
+    list_display = ('id', 'member_name', 'year', 'month', 'working_hours', 'working_days', 'base_salary', 'weekly_holiday_allowance', 'gross_pay', 'net_pay', 'sum_of_deductions')
 
     def member_name(self, obj):
         return obj.member.business.license_name + '_' + obj.member.user.name
@@ -47,3 +63,5 @@ admin.site.register(Business, BusinessAdmin)
 admin.site.register(Member, MemberAdmin)
 admin.site.register(Attendance, AttendanceAdmin)
 admin.site.register(TimeTable, TimeTableAdmin)
+admin.site.register(HolidayPolicy, HolidayPolicyAdmin)
+admin.site.register(PayRoll, PayrollAdmin)
